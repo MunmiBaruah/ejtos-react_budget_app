db8ab708b90d10c520d22b0b615317343c8a1252
Munmi Baruah <munmi@positra.com>
2024-02-14 14:01:16 +0530

    Added all the changes asked in the budget project

--- src/App.js
+++ src/App.js
@@ -4,40 +4,69 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
+import Remaining from "./components/Remaining";
 
 // Add code to import the other components here under
 
 
 import { AppProvider } from './context/AppContext';
+import ExpenseTotal from "./components/ExpenseTotal";
+import CurrencyList from "./components/CurrencyList";
+import ExpenseList from "./components/ExpenseList";
+import AllocationForm from "./components/AllocationForm";
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
+                        <div className='row mt-3'>
+                            <div className='col-sm'>
+                                <Budget/>
+                            </div>
+                            <div className='col-sm'>
+                                <Remaining/>
+                            </div>
+                            <div className='col-sm'>
+                                <ExpenseTotal/>
+                            </div>
+                            <div className='col-sm'>
+                                <CurrencyList/>
+                            </div>
+                        </div>
+                        <h3 className='mt-3'>Shopping Cart</h3>
+                        <div className='row '>
+                            <div className='col-sm'>
+                                <ExpenseList />
+                            </div>
+                        </div>
+                        <h3 className='mt-3'>Allocation Form</h3>
+                        <div className='row '>
+                            <div className='col-sm'>
+                                <AllocationForm/>
+                            </div>
+                        </div>
+
 
                         {
                             /* Add Remaining component here*/
-                        }        
+                        }
 
                         {
                             /* Add ExpenseTotal component here */
-                        }        
-                       
+                        }
+
                         {
-                            /* Add ExpenseList component here */
-                        }         
+                            /* Add CurrencyList component here */
+                        }
 
                         {
                             /* Add ExpenseItem component here */
-                        }        
+                        }
 
                         {
                             /* Add AllocationForm component here under */
-                        }        
+                        }
 
                 </div>
             </div>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,32 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { currency, budget, dispatch } = useContext(AppContext);
+
+    const updateBudget = (val) => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: val
+        })
+    }
+    return (
+        <div className='alert alert-primary'>
+            <span>
+                Cart Value: {currency} <input
+                required='required'
+                type='number'
+                min={1}
+                max={20000}
+                id='cost'
+                value={budget}
+                style={{size: 20}}
+                onChange={(event) => updateBudget(event.target.value)}>
+                        </input>
+
+            </span>
+        </div>
+    )
+}
+
+export default Budget;
